39dd5325d5dbe06915d200bdc51cba60e3f85e45
Smilla Mispelbaum <smmis26@student.sdu.dk>
2026-09-15 13:36:42 +0200

    Update SmillaTest.cs

--- SmillaTest.cs
+++ SmillaTest.cs
@@ -2,6 +2,6 @@ Console.WriteLine("Hello World");
 
 Sring name = Smilla;
 int age = 19;
-bool experience = false;
-Console.WriteLine("Name: " + name + " Age: " + age + " Experience: " + bool + ".");
+
+Console.WriteLine("Name: " + name + " Age: " + age + ".");
 
